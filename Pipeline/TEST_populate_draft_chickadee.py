b173db3ce6c937f56858e5dead8f07f81ae26b3b
TitBro <titouan.brossy@epfl.ch>
2023-11-14 16:04:54 +0100

    Update TEST_populate_draft_chickadee.py

--- Pipeline/TEST_populate_draft_chickadee.py
+++ Pipeline/TEST_populate_draft_chickadee.py
@@ -68,12 +68,10 @@ def test_populate():
 
     logger.info(test_brg_results)
 
-    """
     test_inference = spim.Inference()
     test_inference.populate()
 
     logger.info(test_inference)
-    """
 
     """
     test_stats = (

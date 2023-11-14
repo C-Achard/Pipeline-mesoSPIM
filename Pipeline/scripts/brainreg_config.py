4bc46e67de67728dffa26a753e412128bf7fdb4c
TitBro <titouan.brossy@epfl.ch>
2023-11-14 10:27:58 +0100

    Update brainreg_config.py

--- Pipeline/scripts/brainreg_config.py
+++ Pipeline/scripts/brainreg_config.py
@@ -1,28 +1,28 @@
 import json
 from pathlib import Path
 
-op = Path().resolve() / Path("output_brainreg")
+op = Path().resolve() / Path("output_brainreg_chickadee")
 if not (op.is_dir()):
     op.mkdir()
 
 dictionary = {
     "output_directory": str(op),
     "additional_images": [],
-    "atlas": "example_mouse_100um",
+    "atlas": "allen_mouse_25um",
     "n_free_cpus": 4,
     "brain_geometry": "full",
-    "voxel_sizes": [40, 48, 48],
-    "orientation": "spl",
+    "voxel_sizes": [5, 5.26, 5.26],
+    "orientation": "sal",
     "preprocessing": "default",
     "sort_input_file": False,
     "save_orientation": False,
     "debug": False,
-    "affine_n_steps": 1,
-    "affine_use_n_steps": 1,
-    "freeform_n_steps": 1,
-    "freeform_use_n_steps": 1,
+    "affine_n_steps": 6,
+    "affine_use_n_steps": 5,
+    "freeform_n_steps": 6,
+    "freeform_use_n_steps": 4,
     "bending_energy_weight": 0.95,
-    "grid_spacing": 5,
+    "grid_spacing": 10,
     "smoothing_sigma_reference": 1.0,
     "smoothing_sigma_floating": 1.0,
     "histogram_n_bins_floating": 128,

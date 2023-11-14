3de4ddaa45f7496e8756db88ebe31a55fc85cabe
TitBro <titouan.brossy@epfl.ch>
2023-11-14 10:38:26 +0100

    Create TEST_populate_draft_chickadee.py

---
+++ Pipeline/TEST_populate_draft_chickadee.py
@@ -0,0 +1,92 @@
+ls
+import sys
+import logging
+from pathlib import Path
+import login
+
+sys.path.append("scripts")
+sys.path.append("schema")
+
+login.connectToDatabase()
+from schema import mice, spim, user
+from scripts import *
+
+# USER_PATH = Path.home()
+SCAN_PATH = Path("/Data/seb/CFOS_exp")
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+def test_populate():
+    """Populate all tables as test."""
+    mice.Mouse().insert1(
+        ("mouse", 0, "2022-01-01", "U", "WT"), skip_duplicates=True
+    )
+    user.User().insert1(
+        ("cyril", "cyril.achard@epfl.ch"), skip_duplicates=True
+    )
+
+    test_scan = spim.Scan()
+
+    cfos_path = SCAN_PATH / Path(
+        "CHICKADEE_Mag1.25x_Tile0_Ch561_Sh0_Rot0.tiff"
+    )
+    autofluo_path = SCAN_PATH / Path(
+        "CHICKADEE_Mag1.25x_Tile0_Ch488_Sh0_Rot0.tiff"
+    )
+    logger.info(f"File for cFOS : {cfos_path}")
+    logger.info(f"File for autofluo : {autofluo_path}")
+    time = "2022-01-01 16:16:16"
+
+    test_scan.insert1(
+        (
+            "mouse",
+            0,
+            "cyril",
+            autofluo_path,
+            cfos_path,
+            time,
+        ),
+        skip_duplicates=True,
+    )
+    test_scan_part = spim.Scan.ROI_list()
+    test_scan_part.insert1(("mouse", 0, [656, 962, 767]), skip_duplicates=True)
+
+    logger.info(test_scan)
+
+    test_brainreg = spim.BrainRegistration()
+    test_brainreg.populate()
+
+    logger.info(test_brainreg)
+
+    test_brg_results = spim.BrainRegistrationResults()
+    test_brg_results.populate()
+
+    logger.info(test_brg_results)
+
+    """
+    test_inference = spim.Inference()
+    test_inference.populate()
+
+    logger.info(test_inference)
+    """
+
+    """
+    test_stats = (
+        spim.Analysis()
+    )  # stats on instance seg. : sphericity, centroids, etc...
+    test_stats.populate()
+
+    logger.info(test_stats)
+
+    test_report = spim.Report()
+    test_report.populate()
+
+    logger.info(test_report)
+    """
+
+
+if __name__ == "__main__":
+    login.connectToDatabase()
+    test_populate()

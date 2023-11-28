86c23722b15edece948f872ab625d1ecaef81642
Cyril Achard <94955160+C-Achard@users.noreply.github.com>
2023-11-28 10:29:04 +0100

    Streamlit and docs (#7)
    * Turn off nb exec and ignore built docs
    
    * Refactor params in SL app

--- Pipeline/user_interface.py
+++ Pipeline/user_interface.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 import sys
 import logging
 from pathlib import Path
@@ -8,7 +9,18 @@ import determine_ids
 sys.path.append("scripts")
 sys.path.append("schema")
 
-login.connectToDatabase()
+try:
+    login.connectToDatabase()
+except Exception as e:
+    st.error(
+        "Could not connect to the database.\n Troubleshoothing steps:\n"
+        + "- Make sure the datajoint docker container is running.\n"
+        + "\t - If not, run `docker-compose up` where the database compose file is located.\n"
+        + "- Check your credentials in login.py.\n"
+        + "\t - Check that the IP and password are correct.\n"
+        + "- Contact your network administrator."
+    )
+    st.stop()
 
 from schema import mice, spim, user
 from scripts import brainreg_config
@@ -55,104 +67,109 @@ def main():
     st.title("DataJoint pipeline for brain registration and segmentation")
 
     st.header("Parameters of the brain registration")
-    param1 = st.text_input(
-        "path to output directory for brain registration",
+    brainreg_result_path = st.text_input(
+        "Path to output directory for brain registration",
         value=Path("").resolve(),
     )
-    param2 = []
-    param3 = st.selectbox(
+    additional_files_paths = (
+        []
+    )  # used to list all additional files, asks to user to input all of them depending on the number of files
+    atlas_name = st.selectbox(
         "atlas name",
         ["allen_mouse_50um", "allen_mouse_25um", "allen_mouse_10um"],
     )
-    param3_1 = st.number_input(
+    cpus_number = st.number_input(
         "number of free CPUs", value=4, min_value=0, step=1, format="%d"
     )
-    param4 = st.selectbox(
-        "brain geometry", ["full", "hemisphere_l", "hemisphere_r"]
+    brain_geom = st.selectbox(
+        "Brain geometry", ["full", "hemisphere_l", "hemisphere_r"]
     )
-    param5 = st.number_input("voxel size x", step=0.01, format="%f")
-    param6 = st.number_input("voxel size y", step=0.01, format="%f")
-    param7 = st.number_input("voxel size z", step=0.01, format="%f")
-    param8 = st.text_input("orientation", value="sar")
-    param8_1 = st.text_input("preprocessing", value="default")
-    param9 = st.checkbox("sort input file", value=False)
-    param10 = st.checkbox("save orientation", value=False)
-    param11 = st.checkbox("debug", value=False)
-    param12 = st.number_input(
-        "affine downsampling steps", value=6, min_value=0, step=1, format="%d"
+    voxel_size_x = st.number_input("Voxel size x", step=0.01, format="%f")
+    voxel_size_y = st.number_input("Voxel size y", step=0.01, format="%f")
+    voxel_size_z = st.number_input("Voxel size z", step=0.01, format="%f")
+    orientation_str = st.text_input("Orientation", value="sar")
+    preprocessing_params = st.text_input("Preprocessing", value="default")
+    sort_input_bool = st.checkbox("Sort input file", value=False)
+    save_orientation = st.checkbox("Save orientation", value=False)
+    st.header("Advanced parameters of the brain registration")
+    debug = st.checkbox("Debug", value=False)
+    affine_downsampling_steps = st.number_input(
+        "Affine downsampling steps", value=6, min_value=0, step=1, format="%d"
     )
-    param13 = st.number_input(
-        "affine downsampling steps to use",
+    affine_sampling_steps = st.number_input(
+        "Number of ownsampling steps to use for affine registration",
         value=5,
         min_value=0,
         step=1,
         format="%d",
     )
-    param14 = st.number_input(
-        "freeform downsampling steps",
+    num_freeform_downsample = st.number_input(
+        "Number of freeform downsampling steps",
         value=6,
         min_value=0,
         step=1,
         format="%d",
     )
-    param15 = st.number_input(
-        "freeform downsampling steps to use",
+    number_freeform_downsample_to_use = st.number_input(
+        "Number of downsampling steps to use for freeform registration",
         value=4,
         min_value=0,
         step=1,
         format="%d",
     )
-    param16 = st.number_input(
-        "bending energy weight", value=0.95, step=0.01, format="%f"
+    bending_energy_weight = st.number_input(
+        "Bending energy weight", value=0.95, step=0.01, format="%f"
     )
-    param17 = st.number_input(
-        "grid spacing", value=-10.00, step=0.01, format="%f"
+    grid_spacing = st.number_input(
+        "Grid spacing", value=-10.00, step=0.01, format="%f"
     )
-    param18 = st.number_input(
-        "smoothing sigma reference", value=1.0, step=0.1, format="%f"
+    smoothing_sigma_ref = st.number_input(
+        "Smoothing sigma reference", value=1.0, step=0.1, format="%f"
     )
-    param19 = st.number_input(
-        "smoothing sigma floating", value=1.0, step=0.1, format="%f"
+    smoothing_sigma_floating = st.number_input(
+        "Smoothing sigma floating", value=1.0, step=0.1, format="%f"
     )
-    param20 = st.number_input(
-        "number histogram bins floating", value=128, step=1, format="%d"
+    num_hist_bins = st.number_input(
+        "Number histogram bins floating", value=128, step=1, format="%d"
     )
-    param21 = st.number_input(
-        "number histogram bins reference", value=128, step=1, format="%d"
+    num_hist_bins_ref = st.number_input(
+        "Number histogram bins reference", value=128, step=1, format="%d"
     )
 
     st.header("Parameters of the mouse")
-    param23 = st.checkbox("add new attempt", value=False, key=1)
-    param22 = st.text_input("mouse name", value="chickadee")
-    param24 = st.text_input("date (yyyy-mm-dd)", value="2023-11-11")
-    param25 = st.selectbox("sex", ["M", "F", "U"])
-    param26 = st.text_input("strain", value="WT")
+    add_new_attempt = st.checkbox("Add new attempt", value=False, key=1)
+    mouse_name = st.text_input("Mouse name", value="chickadee")
+    date_of_mouse = st.text_input("Date (yyyy-mm-dd)", value="2023-11-11")
+    mouse_sex = st.selectbox("Sex", ["M", "F", "U"])
+    mouse_strain = st.text_input("Strain", value="WT")
 
     st.header("Parameters of the user")
-    param27 = st.text_input("name", value="cyril")
-    param28 = st.text_input("email", value="cyril.achard@epfl.ch")
+    username = st.text_input("Name", value="cyril")
+    useremail = st.text_input("Email", value="cyril.achard@epfl.ch")
 
     st.header("Parameters of the brain scan")
-    param29 = st.checkbox("add new attempt", value=False, key=2)
-    param30 = st.text_input(
-        "autofluo scan path", value=Path("").resolve() / Path("autofluo.tiff")
+    add_new_parameters_brain_scan = st.checkbox(
+        "Add new attempt", value=False, key=2
     )
-    param31 = st.text_input(
+    autofluo_scan_path = st.text_input(
+        "Autofluo scan path", value=Path("").resolve() / Path("autofluo.tiff")
+    )
+    cfos_scan_path = st.text_input(
         "cFOS scan path", value=Path("").resolve() / Path("cfos.tiff")
     )
-    param32 = st.text_input(
-        "time (yyyy-mm-dd h:m:sec)", value="2023-11-11 16:16:16"
+    scan_time_stamp = st.text_input(
+        "Time (yyyy-mm-dd h:m:sec)", value="2023-11-11 16:16:16"
     )
-    param33 = st.selectbox(
+    rois_choice = st.selectbox(
         "ROIs",
         [
-            "directly give ROI ids",
-            "give ROI exact names",
-            "give ROI global names (ex, retrosplenial)",
+            "Give ROI ids",
+            "Give ROI exact names",
+            "Give ROI global names (ex, retrosplenial)",
         ],
     )
     rois_ids = []
-    if param33 == "directly give ROI ids":
+    if rois_choice == "Give ROI ids":
         numbers_input = st.text_input("ROIs", value="656 - 962 - 767")
         if numbers_input:
             try:
@@ -163,7 +180,7 @@ def main():
                 st.error(
                     "Please enter a valid list of numbers separated by -."
                 )
-    if param33 == "give ROI exact names":
+    if rois_choice == "Give ROI exact names":
         strings_input = st.text_input(
             "ROIs",
             value="Primary motor area, Layer 6a - Primary motor area, Layer 6b",
@@ -175,9 +192,9 @@ def main():
             except ValueError:
                 st.error("Please enter a valid list of ROIs separated by -.")
         rois_ids = determine_ids.extract_ids_of_selected_areas(
-            atlas_name=param3, list_atlas_names=gn
+            atlas_name=atlas_name, list_atlas_names=gn
         )
-    if param33 == "give ROI global names (ex, retrosplenial)":
+    if rois_choice == "Give ROI global names (ex, retrosplenial)":
         strings_input = st.text_input("ROIs", value="Primary motor area")
         gn = []
         if strings_input:
@@ -186,51 +203,51 @@ def main():
             except ValueError:
                 st.error("Please enter a valid list of ROIs separated by -.")
         rois_ids = determine_ids.extract_ids_of_selected_areas(
-            atlas_name=param3, list_global_names=gn
+            atlas_name=atlas_name, list_global_names=gn
         )
 
     if st.sidebar.button("RUN PIPELINE"):
         st.sidebar.write("Starting pipeline")
         params = {
-            "output_directory": Path(param1).resolve(),
-            "additional_images": param2,
-            "atlas": param3,
-            "n_free_cpus": param3_1,
-            "brain_geometry": param4,
-            "voxel_sizes": [param5, param6, param7],
-            "orientation": param8,
-            "preprocessing": param8_1,
-            "sort_input_file": param9,
-            "save_orientation": param10,
-            "debug": param11,
-            "affine_n_steps": param12,
-            "affine_use_n_steps": param13,
-            "freeform_n_steps": param14,
-            "freeform_use_n_steps": param15,
-            "bending_energy_weight": param16,
-            "grid_spacing": param17,
-            "smoothing_sigma_reference": param18,
-            "smoothing_sigma_floating": param19,
-            "histogram_n_bins_floating": param20,
-            "histogram_n_bins_reference": param21,
+            "output_directory": Path(brainreg_result_path).resolve(),
+            "additional_images": additional_files_paths,
+            "atlas": atlas_name,
+            "n_free_cpus": cpus_number,
+            "brain_geometry": brain_geom,
+            "voxel_sizes": [voxel_size_x, voxel_size_y, voxel_size_z],
+            "orientation": orientation_str,
+            "preprocessing": preprocessing_params,
+            "sort_input_file": sort_input_bool,
+            "save_orientation": save_orientation,
+            "debug": debug,
+            "affine_n_steps": affine_downsampling_steps,
+            "affine_use_n_steps": affine_sampling_steps,
+            "freeform_n_steps": num_freeform_downsample,
+            "freeform_use_n_steps": number_freeform_downsample_to_use,
+            "bending_energy_weight": bending_energy_weight,
+            "grid_spacing": grid_spacing,
+            "smoothing_sigma_reference": smoothing_sigma_ref,
+            "smoothing_sigma_floating": smoothing_sigma_floating,
+            "histogram_n_bins_floating": num_hist_bins,
+            "histogram_n_bins_reference": num_hist_bins_ref,
         }
-        attempt = fetch_attempt_mouse(param22)
+        attempt = fetch_attempt_mouse(mouse_name)
         if not attempt:
             attempt = 0
         else:
-            if param23:
+            if add_new_attempt:
                 attempt = np.max(attempt) + 1
             else:
                 attempt = np.max(attempt)
-        scan_attempt = fetch_attempt_scan(param22)
+        scan_attempt = fetch_attempt_scan(mouse_name)
         if not scan_attempt:
             scan_attempt = 0
         else:
-            if param29:
+            if add_new_parameters_brain_scan:
                 scan_attempt = np.max(scan_attempt) + 1
             else:
                 scan_attempt = np.max(scan_attempt)
-        list_ids = return_all_list(param22, scan_attempt)
+        list_ids = return_all_list(mouse_name, scan_attempt)
         attempt_roi = 0
         for key in list_ids:
             if compare_lists(list_ids[key], rois_ids):
@@ -242,29 +259,30 @@ def main():
 
         st.sidebar.write("Populating Mouse table")
         mice.Mouse().insert1(
-            (param22, attempt, param24, param25, param26), skip_duplicates=True
+            (mouse_name, attempt, date_of_mouse, mouse_sex, mouse_strain),
+            skip_duplicates=True,
         )
 
         st.sidebar.write("Populating User table")
-        user.User().insert1((param27, param28), skip_duplicates=True)
+        user.User().insert1((username, useremail), skip_duplicates=True)
 
         st.sidebar.write("Populating Scan table")
         scan = spim.Scan()
 
-        cfos_path = Path(param31)
-        autofluo_path = Path(param30)
+        cfos_path = Path(cfos_scan_path)
+        autofluo_path = Path(autofluo_scan_path)
 
         logger.info(f"File for cFOS : {cfos_path}")
         logger.info(f"File for autofluo : {autofluo_path}")
 
         scan.insert1(
             (
-                param22,
+                mouse_name,
                 scan_attempt,
-                param27,
+                username,
                 autofluo_path,
                 cfos_path,
-                param32,
+                scan_time_stamp,
             ),
             skip_duplicates=True,
         )

d7c807728ee1ad9602489859e17076f4d9fac947
TitBro <titouan.brossy@epfl.ch>
2023-11-29 16:40:48 +0100

    Update plot_schema.py

--- Pipeline/scripts/plot_schema.py
+++ Pipeline/scripts/plot_schema.py
@@ -1,7 +1,7 @@
 import os
 import datajoint as dj
 import login
-
+import matplotlib.pyplot as plt
 import sys
 
 sys.path.append("./scripts")
@@ -10,5 +10,8 @@ sys.path.append("./schema")
 login.connectToDatabase()
 from schema import mice, spim, user
 
-d = dj.ERD(spim)
-d.save("diagram.png")
+my_erd = dj.ERD(spim)
+fig = my_erd.draw()
+
+# Save the figure
+fig.savefig("diagram.png")

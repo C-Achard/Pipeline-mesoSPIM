c0f2426be5a8efbe87503c173eca18320db21581
TitBro <titouan.brossy@epfl.ch>
2023-12-01 14:11:10 +0100

    modifs

--- Pipeline/test.py
+++ Pipeline/test.py
@@ -51,19 +51,3 @@ def display_cropped_continuous_cfos_napari(
         table["cont_region_id"]: imio.load_any(table["instance_labels"])
         for table in query_instance
     }
-    viewer = naparari.Viewer()
-    for key in Masks:
-        sample_space = np.zeros_like(Masks)
-        sample_space[
-            Masks[key][1] : Masks[key][2] + 1,
-            Masks[key][3] : Masks[key][4] + 1,
-            Masks[key][5] : Masks[key][6] + 1,
-        ] = Instance_labels[key]
-        viewer.add_labels(sample_space[Masks[key][0]])
-
-
-if __name__ == "__main__":
-    login.connectToDatabase()
-    display_cropped_continuous_cfos_napari(
-        "mouse_chickadee", "cyril_tit", 0, 0
-    )
